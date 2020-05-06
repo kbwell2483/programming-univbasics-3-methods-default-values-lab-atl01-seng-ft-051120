a8e8a8f4ad429260a0924374f17852a3465939c0
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-05-06 00:47:18 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,3 +1,5 @@
 meal_choice(veg1, veg2, protein = tofu)
-puts "What a nutritious meal!"
-puts "A plavet of #{protein} with #{veg1} and #{veg2}."
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+
+meal_choice(carrots, string beans, lentils)
